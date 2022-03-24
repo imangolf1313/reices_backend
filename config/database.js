4a80fd9075e771b59cd842872819028355a7fa5e
imangolf1313 <osvaldo.lopez.is@unipolidgo.edu.mx>
2022-03-24 02:29:57 -0600

    ssl

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL"),
+      ssl: env.bool("DATABASE_SSL", false),
     },
   },
 });
